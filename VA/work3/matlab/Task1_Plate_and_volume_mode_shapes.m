419d6852e35fb37a47cce12998487cb358603684
lucas-schroeder <lucasschroeder.ss@gmail.com>
2021-10-24 22:00:06 -0300

    Work3 almost done

--- VA/work3/matlab/Task1_Plate_and_volume_mode_shapes.m
+++ VA/work3/matlab/Task1_Plate_and_volume_mode_shapes.m
@@ -7,12 +7,12 @@ close all; clc; clear;
 %% Part 1 - Plate mode shape
 fprintf('Solving door model\n')
 door_param = containers.Map;
-door_param('h') = 1.6e-3;  % Thickness [m]
+door_param('h') = 2e-3;  % Thickness [m]
 door_param('H') = 500e-3;  % Door height [m]
 door_param('L') = 200e-3;  % Door length [m]
-door_param('E') = 186e9;   % Modulus of elasticity [Pa]
+door_param('E') = 200e9;   % Modulus of elasticity [Pa]
 door_param('nu') = 0.3;    % Poison coef
-door_param('rho') = 7870;  % Steel density [kg/m**3]
+door_param('rho') = 7850;  % Steel density [kg/m**3]
 door_param('eSize') = 0.02; % Element size [m]
 
 door = PlateModel(door_param);
@@ -21,26 +21,35 @@ door = door.generate_mesh(door);
 door = door.get_global_matrices(door);
 
 % Fixed regions (Two-points recatangles [x1 z1, x2 z2])
-regions=containers.Map;
-regions('1')=[0.000 0.100, 0.020 0.120]; % Lower hinge
-regions('2')=[0.000 0.380, 0.020 0.400]; % Upper hinge
-regions('3')=[0.180 0.260, 0.220 0.280]; % Knob
+regions = containers.Map;
+regions('1') = [0.000 0.100, 0.020 0.120]; % Lower hinge
+regions('2') = [0.000 0.380, 0.020 0.400]; % Upper hinge
+regions('3') = [0.180 0.260, 0.220 0.280]; % Knob
 
 door = door.apply_bc(door, regions);
 door = door.solve_eigenvalue_problem(door, 50);
+fprintf('Done!\n')
 
-%% 1.1 - Plot Door mode shapes
+%% 1.1 - Plot door mode shapes (decoupled)
 x = 0 : door.dx : door.L;
 z = 0 : door.dz : door.H;
 [X, Z] = meshgrid(x, z);
-for mode=1:5
-    figure()
-    mode_shape = door.results.Vc(:,mode);
+figure()
+for mode=1:6
+    subplot(2, 3, mode)
+    mode_shape = door.results.Vc(:, mode);    
+    mode_shape = 0.02.*mode_shape./max(mode_shape); %scale
     mode_shape = reshape(mode_shape, door.nNodesX, door.nNodesZ);
-    surf(X, Z, mode_shape.');
-    grid on
+    surf(X, mode_shape.', Z, mode_shape.');
+    title(sprintf('Mode %.f (%0.1f [Hz])',mode,door.results.fn(mode)))
+    xlabel('X [m]');
+    zlabel('Z [m]');
+    set(gca,'ytick',[])
+    axis equal;
+    grid on;
     box on;
 end
+
 
 %% Part 2 - Solve acoutic cavity model
 fprintf('Solving acoustic cavity model\n')
@@ -49,43 +58,49 @@ cvt_param = containers.Map;
 cvt_param('W') = 240e-3; % volume width [m] (X axis
 cvt_param('D') = 240e-3; % volume depth [m] (Y axis)
 cvt_param('H') = 540e-3; % volume height [m] (Z axis)
-cvt_param('c') = 348; % sound speed [m/s]
-cvt_param('rho') = 1.7; % density [kg/m**3]
+cvt_param('c') = 346.25; % sound speed [m/s]
+cvt_param('rho') = 1.225; % density [kg/m**3]
 cvt_param('eSize') = 0.02; % element size [m]
 
 cvt = AcousticModel(cvt_param);
 cvt = cvt.get_element_matrices(cvt);
 cvt = cvt.generate_mesh(cvt);
 cvt = cvt.get_global_matrices(cvt);
-cvt = cvt.solve_eigenvalue_problem(cvt, 5);
+cvt = cvt.solve_eigenvalue_problem(cvt, 25);
+fprintf('Done!\n')
 
 
-%% 2.1 - Plot decoupled acoustic cavity mode shapes
+%% 2.1 - Plot acoustic cavity mode shapes (decoupled)
 Vc = cvt.results.Phi;
 x = 0 : cvt.dx : cvt.W;
 y = 0 : cvt.dy : cvt.D;
 z = 0 : cvt.dz : cvt.H;
 [X, Y, Z] = meshgrid(x, y, z);
+xslice = [0.000, cvt.W];
+yslice = [0.000, cvt.D];
+zslice = [0.000, cvt.H];
 
-for mode = 1:5    
-    figure()
-    set(gcf, 'Position', get(0, 'Screensize'));
+figure()
+for mode = 1:6
+    subplot(2, 3, mode)
     mode_shape = reshape(Vc(:,mode), [cvt.nNodesX, cvt.nNodesY, cvt.nNodesZ]);
     mode_shape = permute(mode_shape,[2 1 3]);
-    slice(X,Y,Z, mode_shape, 0, 0, 0.540);
+    slice(X,Y,Z, mode_shape, xslice, yslice, zslice);
     box on
     ax = gca;
     ax.BoxStyle = 'full';
     shading interp
-    set(gca, 'FontSize', 20)
-    xlabel('Width [m]')
-    ylabel('Length [m]')
-    zlabel('Height [m]')
-    title(sprintf('Decoupled acoustic cavity - Mode %.f (%0.1f [Hz])',mode,cvt.results.fn(mode)));
-    colormap jet;colorbar
+    xlabel('X [m]')
+    ylabel('Y [m]')
+    zlabel('Z [m]')
+    title(sprintf('Mode %.f (%0.1f [Hz])',mode,cvt.results.fn(mode)));
+    colormap jet;
     set(gcf,'color','w');
     axis equal
 end
+c = colorbar;
+c.Label.String = 'Acoustic Pressure';
+c.Position = [0.9 0.1 0.03 0.8];
 
 %% Part 3 - Solve acoutic cavity with back opening
 fprintf('Solve acoutic cavity with back opening\n')
@@ -100,7 +115,8 @@ regions = containers.Map;
 regions('1')=[0.180 0.240 0.040; 0.220 0.240 0.100]; % back opening
 
 cvt2 = cvt2.apply_bc(cvt2, regions);
-cvt2 = cvt2.solve_eigenvalue_problem(cvt2, 5);
+cvt2 = cvt2.solve_eigenvalue_problem(cvt2, 25);
+fprintf('Done!\n')
 
 %% 3.1 - Plot
 Vc = cvt2.results.Phi;
@@ -108,15 +124,15 @@ x = 0 : cvt.dx : cvt.W;
 y = 0 : cvt.dy : cvt.D;
 z = 0 : cvt.dz : cvt.H;
 [X, Y, Z] = meshgrid(x, y, z);
+xslice = [0.000, cvt.W];
+yslice = [0.000, cvt.D];
+zslice = [0.000, cvt.H];
 
-for mode = 1:5    
-    figure()
-    set(gcf, 'Position', get(0, 'Screensize'));
+figure()
+for mode = 1:6    
+    subplot(2,3,mode)
     mode_shape = reshape(Vc(:,mode), [cvt.nNodesX, cvt.nNodesY, cvt.nNodesZ]);
     mode_shape = permute(mode_shape,[2 1 3]);
-    xslice = [0.000, 0.240];   
-    yslice = [0.000, 0.240];
-    zslice = [0.000, 0.540];
     slice(X,Y,Z, mode_shape, xslice, yslice, zslice);
     box on;
     grid on;
@@ -124,16 +140,20 @@ for mode = 1:5
     ax.BoxStyle = 'full';
     shading interp
     set(gca, 'FontSize', 20)
-    xlabel('Width [m]')
-    ylabel('Length [m]')
-    zlabel('Height [m]')
-    title(sprintf('Decoupled acoustic cavity - Mode %.f (%0.1f [Hz])',mode,cvt.results.fn(mode)));
-    colormap jet;colorbar
+    xlabel('X [m]')
+    ylabel('Y [m]')
+    zlabel('Z [m]')
+    title(sprintf('Mode %.f (%0.1f [Hz])',mode,cvt.results.fn(mode)));
+    colormap jet;
     set(gcf,'color','w');
     axis equal
 end
+c = colorbar;
+c.Label.String = 'Acoustic Pressure';
+c.Position = [0.9 0.1 0.03 0.8];
 
 %% Part 4 - Strong-cloupling
+fprintf('Calculating coupling matrix\n')
 a = door.dx;
 b = door.dz;
 A_e = [1  -1  -1   1   1    1  -1   -1   -1  -1    1    1 ;
@@ -263,6 +283,7 @@ K_coupled = [   K      S   ;
 K_coupled = sparse(K_coupled);
 
 %% 4.1 - Solving the eigenvalue problem
+fprintf('Solving the coupled system eigenvalue problem\n');
 [Vc, Wn2] = eigs(K_coupled, M_coupled, 10, 'sm');
 fn = diag(Wn2).^(0.5)/(2*pi); % in Hertz
 % Ordering eigenvalues and eigenvectors
@@ -279,6 +300,7 @@ m_r = 1./(m_r.^0.5);
 for i = 1:size(Vc,2)
     Vc(:,i) = Vc(:,i).*m_r(i);
 end
+fprintf('Done!\n');
 
 %% 4.2 - Splitting the DoF vector
 Vc_door = Vc(1:length(door.M), :);
@@ -294,51 +316,66 @@ Vc_door = real(Vc_door(1:3:end,:)); % Displacement shape (dof 1,4,7,10,13,...)
 % Taking the cavity results
 Vc_cvt = Vc(length(door.M)+1:end, :);
 
-%% 4.3 - Plot Door mode shapes
+%% 4.3 - Plot door mode shapes (coupled)
 x = 0 : door.dx : door.L;
 z = 0 : door.dz : door.H;
 [X, Z] = meshgrid(x, z);
-for mode = 1:5
-    figure()
+figure()
+for mode = 1:6
+    subplot(2,3,mode)
     mode_shape = Vc_door(:,mode);
+    mode_shape = 0.02.*mode_shape./max(mode_shape); %scale
     mode_shape = reshape(mode_shape, door.nNodesX, door.nNodesZ);
-    surf(X, Z, mode_shape.');
+    surf(X, mode_shape.', Z, mode_shape.');
+    title(sprintf('Mode %.f (%0.1f [Hz])',mode,door.results.fn(mode)))
+    xlabel('X [m]');
+    zlabel('Z [m]');
+    set(gca,'ytick',[])
+    axis equal;
     grid on;
     box on;
 end
 
-%% 4.4 - Plot Cavity mode shapes
+%% 4.4 - Plot acoustic cavity mode shapes (coupled)
 x = 0 : cvt.dx : cvt.W;
 y = 0 : cvt.dy : cvt.D;
 z = 0 : cvt.dz : cvt.H;
 [X, Y, Z] = meshgrid(x, y, z);
+xslice = [0.000, cvt.W];
+yslice = [0.000, cvt.D];
+zslice = [0.000, cvt.H];
 
-for mode = 1:5    
-    figure()
-    set(gcf, 'Position', get(0, 'Screensize'));
+figure()
+for mode = 1:6    
+    subplot(2,3,mode)
     mode_shape = reshape(Vc_cvt(:,mode), [cvt.nNodesX, cvt.nNodesY, cvt.nNodesZ]);
     mode_shape = permute(mode_shape,[2 1 3]);
-    slice(X,Y,Z, mode_shape, 0, 0, 0.540);
-    
-    box on
+    slice(X,Y,Z, mode_shape, xslice, yslice, zslice);
+    box on;
+    grid on;
     ax = gca;
     ax.BoxStyle = 'full';
     shading interp
     set(gca, 'FontSize', 20)
-    xlabel('Width [m]')
-    ylabel('Length [m]')
-    zlabel('Height [m]')
-    title(sprintf('Decoupled acoustic cavity - Mode %.f (%0.1f [Hz])',mode,cvt.results.fn(mode)));
-    colormap jet;colorbar
+    xlabel('X [m]')
+    ylabel('Y [m]')
+    zlabel('Z [m]')
+    title(sprintf('Mode %.f (%0.1f [Hz])',mode,cvt.results.fn(mode)));
+    colormap jet;
     set(gcf,'color','w');
     axis equal
 end
+c = colorbar;
+c.Label.String = 'Acoustic Pressure';
+c.Position = [0.9 0.1 0.03 0.8];
 
 %% 4.5 - Compute FRF
 % Find nodes and DoF of interest
+fprintf('Computing FRF\n')
 A_coor = [0.160 0.420]; % Force at the door. Door csys [x z]
 B_coor = [0.100 0.160 0.340]; % Response inside cavity. Cavity csys [x y z]
 
+%%%% 
 x = door.node_coor(:,1) == A_coor(1);
 z = door.node_coor(:,2) == A_coor(2);
 idx = x & z;
@@ -349,6 +386,7 @@ else
 end
 A_dof = door.node_dof(A_node,2); % In door csys (also in coupled-system csys)
 
+%%%%
 x = cvt.node_coor(:,1) == B_coor(1);
 y = cvt.node_coor(:,2) == B_coor(2);
 z = cvt.node_coor(:,3) == B_coor(3);
@@ -361,71 +399,55 @@ end
 B_dof = cvt.node_dof(B_node,2); % In cavity csys
 B_dof_coupled_csys = B_dof + length(M); % In coupled-system csys
 
-% % FRF (m/N)
-% % coupled
-% % only one response point
-% eta=0.03;
-% freq = 10:0.5:500;
-% H_jk = zeros(door.ndof/3, length(freq));
-% k = find(door.node_dof(:,2)==A_dof); % DoF index without rotations
-% for j = 1:door.ndof/3 % For each response dof
-%     for p = 1:length(freq) % At a given frequency
-%         for n = 1:length(fn) % Sum the contribution of every mode
-%             H_jk(j, p) = H_jk(j, p) + Vc_door(j, n)*Vc_door(k, n)/...
-%                                     (fn(n)^2 - freq(p)^2 + 1j*eta*fn(n)^2);
-%         end
-%     end
-% end
-
-% FRF (m/N) Decoupled
+%%%% Weak coupling %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FRF between a force at poit A a displacements at every door node
+freq = 10:2:700; % [Hz]
+H_jk = zeros(door.ndof/3, length(freq)); % [m/N]
+V_jk = zeros(door.ndof/3, length(freq)); % [(m/s)/N] 
 eta=0.03;
-freq = 10:0.5:500;
-H_jk = zeros(door.ndof/3, length(freq));
 k = find(door.node_dof(:,2)==A_dof); % DoF index without rotations
+k = int16(k);
 for j = 1:size(door.results.Vc, 1) % For each response dof
     for p = 1:length(freq) % At a given frequency
         for n = 1:length(door.results.fn) % Sum the contribution of every mode
             H_jk(j, p) = H_jk(j, p) + door.results.Vc(j, n)*door.results.Vc(k, n)/...
                                     (door.results.fn(n)^2 - freq(p)^2 + 1j*eta*door.results.fn(n)^2);
+            V_jk(j, p) = 1j*freq(p)*H_jk(j, p);
         end
     end
 end
 
-% FRF (Pa/(m^3/m^3)) Decoupled
-% frf between point B and every other DoF of the cavity
-eta=0.03;
-freq = 10:0.5:500;
+
+% FRF (Pa/(m^3/s/m^3)) between a volumetric acoustic sources at every cavity
+% node in contact with theplate, and point B
 G_jk = zeros(cvt.ndof, length(freq));
+eta=0.03;
 k = find(cvt.node_dof(:,2) == B_dof); % DoF index without rotations
+k = int16(k);
 for j = 1:size(cvt.results.Phi, 1) % For each response dof
     for p = 1:length(freq) % At a given frequency
         for n = 1:length(cvt.results.fn) % Sum the contribution of every mode
             G_jk(j, p) = G_jk(j, p) + cvt.results.Phi(j, n)*cvt.results.Phi(k, n)/...
-                                    (cvt.results.fn(n)^2 - freq(p)^2 + 1j*eta*cvt.results.fn(n)^2);
+                                    (cvt.results.fn(n)^2 - freq(p)^2);
         end
     end
 end
 
-G_jk_door = G_jk(door_cvt_nodes, :); % frf between point B and the DoF at the door (Pa/(m^3/m^3))
+G_jk_door = G_jk(door_cvt_nodes, :); % FRF between point B and the DoFs at the door (Pa/(m^3/m^3))
 vol_element = cvt.dx*cvt.dy*cvt.dz;
 srf_element = cvt.dx*cvt.dz;
-P_jk_door = G_jk_door*vol_element/srf_element; % Pa/m (Pa per unit displacement of element face.
+P_jk_door = G_jk_door*vol_element/srf_element; % Pa/m/s (Pa per unit displacement rate of element face.
 
 I = zeros(size(H_jk));
 for j = 1:size(P_jk_door,1)
-    I(j,:) = H_jk(j,:) .* P_jk_door(j,:);
+    I(j,:) = V_jk(j,:) .* P_jk_door(j,:); % [(m/s)/N]*[Pa/(m/s)] = [Pa/N]
 end
 
-semilogy(sum(real(I), 1))
+total_FRF = sum(real(I), 1);
+p_ref = 20*e-6; % 20 uPa
+total_FRF_db = 20.*log(abs(total_FRF/p_ref));
+fprintf('Done!\n')
 
-% A_jk = zeros(length(freq),1);
-% for p = 1:length(freq)
-%     for n = 1:length(fn)
-%         A_jk(p) = A_jk(p) + Vc(A_dof, n)*Vc(B_dof, n)/(fn(n)^2 - freq(p)^2 + 1j*eta*fn(n)^2);
-%     end
-% end
-% 
-% semilogy(abs(A_jk))
 %% 4.6 - Plot ansys and code
 Ansys_pointA = xlsread('Ansys_pointA.xlsx'); % Meter/Newton
 Ansys_pointA = [Ansys_pointA(:,2) Ansys_pointA(:,3).*exp(1j*deg2rad(Ansys_pointA(:,4)))];
@@ -434,9 +456,11 @@ Ansys_pointB = [Ansys_pointB(:,2) Ansys_pointB(:,3).*exp(1j*deg2rad(Ansys_pointB
 
 figure(); hold on;
 f = real(Ansys_pointB(:,1));
-semilogy(f, abs(Ansys_pointB(:,2)))
-
-semilogy(freq, abs(sum(real(I), 1))*1e-5)
+plot(f, 20.*log(abs(Ansys_pointB(:,2))/p_ref))
+plot(freq, total_FRF_db)
+legend({'Ansys', 'Author'})
+xlabel('Frequency [Hz]') 
+ylabel('SPL [dB] (ref. 20 \muPa)') 
 
 %% Plot [NOT WORKING}
 
@@ -489,6 +513,7 @@ semilogy(freq, abs(sum(real(I), 1))*1e-5)
 %     % Link two axes together
 %     hLink = linkprop([ax1,ax2],{'CameraUpVector','CameraPosition','CameraTarget'});
 %     % Hide the top axes
+%     ax2 = axes;
 %     ax2.Visible = 'off';
 %     ax2.XTick = [];
 %     ax2.YTick = [];
